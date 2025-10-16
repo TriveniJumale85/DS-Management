4e9b056caa46c71e2207dd778ab86948fcc93afb
Triveni Jumale <trivenijumale29@gmail.com>
2025-10-16 14:50:14 +0530

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./pages/navbar/navbar.component";
+import { OurTeacherComponent } from "./our-teacher/our-teacher.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, OurTeacherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './pages/navbar/navbar.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { OurTeacherComponent } from './our-teacher/our-teacher.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    
+     { path: '', component: HomeComponent, title: 'Home | Digital Classroom' },
       { path: 'navbar', component: NavbarComponent},
+       { path: 'student_dashboard', component: StudentDashboardComponent},
+        { path: 'our_teacher', component: OurTeacherComponent},
+        { path: '**', redirectTo: '' }
 ];
