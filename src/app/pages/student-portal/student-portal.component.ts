35be7dc2d44ee9253787422cba24a78fc484a873
Triveni Jumale <trivenijumale29@gmail.com>
2025-11-05 20:11:02 +0530

    changes

--- src/app/pages/student-portal/student-portal.component.ts
+++ src/app/pages/student-portal/student-portal.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
+
 
 @Component({
   selector: 'app-student-portal',
