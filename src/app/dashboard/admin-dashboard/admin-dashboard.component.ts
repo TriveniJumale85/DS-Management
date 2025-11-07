3715b410bb115470effa2ebb28c8fb0a7c0348fe
rutikrokade <rutikrokade6880@gmail.com>
2025-11-07 17:06:39 +0530

    chatbot

--- src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ChatboatComponent } from "../../pages/chatboat/chatboat.component";
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ChatboatComponent],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
--- src/app/dashboard/parent-dashboard/parent-dashboard.component.ts
+++ src/app/dashboard/parent-dashboard/parent-dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { ChatboatComponent } from "../../pages/chatboat/chatboat.component";
 
 @Component({
   selector: 'app-parent-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ChatboatComponent],
   templateUrl: './parent-dashboard.component.html',
   styleUrl: './parent-dashboard.component.css'
 })
