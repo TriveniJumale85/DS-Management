c94b4266f4a43f98bc6efd1b02e1a77fb9995cb5
Triveni Jumale <trivenijumale29@gmail.com>
2025-11-19 10:52:47 +0530

    student dashboard

--- src/app/dashboard/student-dashboard/models/assignment.spec.ts
+++ src/app/dashboard/student-dashboard/models/assignment.spec.ts
@@ -2,6 +2,6 @@ import { Assignment } from './assignment';
 
 describe('Assignment', () => {
   it('should create an instance', () => {
-    expect(new Assignment()).toBeTruthy();
+    expect({} as Assignment).toBeTruthy();
   });
 });
